2208425cb289a85008f51d2a1fa521bdb60888bc
AndrewRSS <andrewrss15@gmail.com>
2020-05-21 17:02:42 -0300

    Adicionado função para atualizar um carro.

--- Demo.Car/Demo.Car/src/main/java/andrew/estudos/demo/car/api/CarrosController.java
+++ Demo.Car/Demo.Car/src/main/java/andrew/estudos/demo/car/api/CarrosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,15 @@ public class CarrosController {
 	
 	@PostMapping
 	public String post(@RequestBody Carro carro) {
-		Carro c = service.save(carro);
+		Carro c = service.insert(carro);
+		
+		return "Carro salvo com sucesso.";
+	}
+	
+	@PutMapping("/{id}")
+	public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+		
+		Carro c = service.update(carro, id);
 		
 		return "Carro salvo com sucesso.";
 	}
