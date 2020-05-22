1dc655ad738b8beebcad81b996c3a364b29052ed
AndrewRSS <andrewrss15@gmail.com>
2020-05-22 08:36:04 -0300

    Incremento do return do http Status 404.

--- Demo.Car/Demo.Car/src/main/java/andrew/estudos/demo/car/api/CarrosController.java
+++ Demo.Car/Demo.Car/src/main/java/andrew/estudos/demo/car/api/CarrosController.java
@@ -32,8 +32,13 @@ public class CarrosController {
 		
 	
 	@GetMapping("/{id}")
-	public Optional<Carro> get(@PathVariable("id") Long id) {
-		return service.getCarrosById(id);
+	public ResponseEntity<Carro> get(@PathVariable("id") Long id) {
+		Optional<Carro> carro = service.getCarrosById(id);
+		if(carro.isPresent()) {
+			return ResponseEntity.ok(carro.get());
+		} else {
+			return ResponseEntity.notFound().buid();
+		}
 	}
 	
 	@GetMapping("/tipo/{tipo}")
